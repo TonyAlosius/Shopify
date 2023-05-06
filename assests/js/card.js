6ea14a150f71e3aeb2b65a14a07afb6d54a29217
Tony Alosius <104986715+TonyAlosius@users.noreply.github.com>
2023-05-06 19:19:26 +0530

    Single section Hosting

---
+++ assests/js/card.js
@@ -0,0 +1,59 @@
+//  Shirts
+var swiper = new Swiper(".mySwiper-shirts", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".bnext-shirts",
+      prevEl: ".bprev-shirts",
+    },
+  });
+
+// // Jeans
+// var swiper = new Swiper(".mySwiper-jeans", {
+//   slidesPerView: 4,
+//   spaceBetween: 10,
+//   slidesPerGroup: 1,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".bnext-jeans",
+//     prevEl: ".bprev-jeans",
+//   },
+// });
+
+// // Pants
+// var swiper = new Swiper(".mySwiper-pants", {
+//   slidesPerView: 4,
+//   spaceBetween: 10,
+//   slidesPerGroup: 1,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".bnext-pants",
+//     prevEl: ".bprev-pants",
+//   },
+// });
+
+// // Tshirts
+// var swiper = new Swiper(".mySwiper-tshirts", {
+//   slidesPerView: 4,
+//   spaceBetween: 10,
+//   slidesPerGroup: 1,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".bnext-tshirts",
+//     prevEl: ".bprev-tshirts",
+//   },
+// });
